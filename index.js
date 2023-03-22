23aa521180928d9712c49be0e5226225f2436552
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-22 01:34:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+
+
+const employee = {
+    name: "Yazeed",
+    streetAddress: "35434525",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee }
+    newObj[key] = value
+    return newObj
+}
+
+updateEmployeeWithKeyAndValue(employee, "dave", "1")
+
+
+function deleteFromEmployeeByKey(employee, key){
+    const remEmployee = { ...employee }
+    let name = key
+    let newEmployee = delete remEmployee[name]
+    return {newEmployee}
+}
+
+deleteFromEmployeeByKey(employee, "somo")
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
